c7352e950efa0656f6d5651ca9189d7af3ca5860
김건 <gimgeon@gimgeon-ui-MacBookAir-2.local>
2025-03-20 16:07:18 +0900

    🎉 Begin: 프로젝트 시작

---
+++ UMC-8th-workbook/UMC-8th-workbook/UMC_8th_workbookApp.swift
@@ -0,0 +1,17 @@
+//
+//  UMC_8th_workbookApp.swift
+//  UMC-8th-workbook
+//
+//  Created by 신연주 on 3/20/25.
+//
+
+import SwiftUI
+
+@main
+struct UMC_8th_workbookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
