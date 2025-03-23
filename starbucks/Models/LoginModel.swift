6df26c8fee56f1ab7b0a8717e3d88de4f3795bc0
김건 <gimgeon@gimgeon-ui-MacBookAir-2.local>
2025-03-23 19:10:41 +0900

    ✨ Feat: 폴더 및 파일 생성

---
+++ starbucks/Models/LoginModel.swift
@@ -0,0 +1,13 @@
+//
+//  LoginModel.swift
+//  starbucks
+//
+//  Created by 신연주 on 3/23/25.
+//
+
+import SwiftUI
+
+struct LoginModel{
+    var id: String
+    var password: String
+}
---
+++ starbucks/Views/SplashView.swift
@@ -0,0 +1,18 @@
+//
+//  SplashView.swift
+//  starbucks
+//
+//  Created by 신연주 on 3/23/25.
+//
+
+import SwiftUI
+
+struct SplashView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SplashView()
+}
