8a444f84ec2b026e857d9ec1cda2f0337b9dc565
김건 <gimgeon@gimgeon-ui-MacBookAir-2.local>
2025-03-24 01:53:09 +0900

    ✨ Feat: LoginView,SplashView 구현

---
+++ starbucks/Views/LoginView.swift
@@ -0,0 +1,80 @@
+//
+//  LoginView.swift
+//  starbucks
+//
+//  Created by 신연주 on 3/23/25.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        VStack{
+            Spacer().frame(height: 104)
+            MainTitleGroup
+            Spacer()
+            MainLoginGroup
+            Spacer()
+            BottomLoginGroup
+            Spacer().frame(height: 62)
+        }
+        .padding(.horizontal, 19)
+    }
+    
+    // 상단 로고
+    private var MainTitleGroup: some View {
+        VStack(alignment:.leading){
+            Image(.starbucksLogo)
+                .resizable()
+                .frame(width:97, height:95)
+            
+            Spacer().frame(height: 28)
+            
+            Text("안녕하세요.")
+            Text("스타벅스입니다.")
+            
+            Spacer().frame(height: 19)
+            
+            Text("회원 서비스 이용을 위해 로그인 해주세요.")
+                .foregroundStyle(Color.gray)
+        }
+        .frame(maxWidth: .infinity, alignment: .leading)
+    }
+    
+    //로그인
+    private var MainLoginGroup: some View{
+        VStack(alignment:.leading){
+            Text("아이디")
+            Divider()
+            Spacer().frame(height: 47)
+            Text("비밀번호")
+            Divider()
+            Button(action: {
+                print("버튼을 클릭했습니다")
+            }, label: {
+                Text("로그인하기")
+                    .foregroundColor(.white)
+            })
+            .frame(maxWidth: .infinity)
+            .padding(.vertical, 13.5)
+            .background(Color(red: 1/255, green: 168/255, blue: 98/255))
+            .cornerRadius(20)
+        }
+    }
+    
+    // 하단 로그인
+    private var BottomLoginGroup: some View{
+        VStack{
+            Text("이메일로 회원가입하기")
+                .underline()
+                .foregroundStyle(Color.gray)
+            Image(.kakaoLogin)
+            Spacer().frame(height: 19)
+            Image(.appleLogin)
+        }
+    }
+}
+
+#Preview {
+    LoginView()
+}
