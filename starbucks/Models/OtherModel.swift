083f8c3675c24bcd4f0e0a39835d5bbd0db1bce1
김건 <gimgeon@gimgeon-ui-MacBookAir-2.local>
2025-04-14 00:29:18 +0900

    [week2] 과제✨ Feat: Other 탭 섹션 구현

---
+++ starbucks/Models/OtherModel.swift
@@ -0,0 +1,15 @@
+//
+//  OtherModel.swift
+//  starbucks
+//
+//  Created by 신연주 on 4/13/25.
+//
+
+import Foundation
+import SwiftUI
+
+struct OtherModel{
+    let menuIcon: Image
+    let menuName: String
+}
+
--- starbucks/Views/OrderView.swift
+++ starbucks/Views/OrderView.swift
@@ -12,41 +12,7 @@ struct OrderView: View {
         Text("OrderView")
     }
 }
-/*
-struct OrderView: View {
-    var body: some View {
- 
-         VStack(spacing: 41){
-         Spacer()
-         MainTitleGroup
-         
-         MainInfoGroup
-         
-         Spacer()
-         
-         PayGroup
-         
-         Spacer()
-         }
-        
-    }
-}
 
-    public var MainTitleGroup: some View{
-        
-    }
-    
-    public var MainInfoGroup: some View {
-        
-    }
-    
-    public var PayGroup: some View{
-        
-    }
-    
-    public var
-}*/
- 
 #Preview {
     OrderView()
 }
